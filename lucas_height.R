793479713570b194eea00f9b7094dfa32c9c71e7
restrellado <ryan.estrellado@gmail.com>
2016-02-28 17:26:45 -0800

    Added colors for teams.

--- lucas_height.R
+++ lucas_height.R
@@ -14,14 +14,20 @@ df2 <- df %>%
   rename("Height" = Height..m.) %>%
   arrange(desc(Height)) %>%
   filter(Club == "Liverpool" | Club == "Manchester City") %>%
-  mutate(Lucas = Player == "Lucas Leiva")
+  mutate(Lucas = Player == "Lucas Leiva") %>%
+  mutate(Color = Club == "Manchester City")
 
 # Plot
-ggplot(df2, aes(x = Height, y = reorder(Player, Height), color = Lucas)) +
+ggplot(df2, aes(x = Height, y = reorder(Player, Height), shape = Lucas,
+                color = Color)) +
   geom_point(size = 3) +
-  scale_color_manual(values = c("dark grey", "hotpink3")) +
-  guides(color = FALSE) +
+  scale_color_manual(values = c("red", "sky blue")) +
+  guides(color = FALSE, shape = FALSE) +
   labs(x = "Height (Meters)", y = "") +
   geom_vline(xintercept = med, linetype = 3, size = 1.5) +
   annotate("text", label = "league median height",
-           x = med + .040, y = "Lucas Leiva", size = 4)
+           x = med + .060, y = "Philippe Coutinho", size = 4) +
+  annotate("text", label = "Lucas",
+           x = (df2$Height[df2$Player == "Lucas Leiva"]) - .020,
+           y = "Lucas Leiva",
+           color = "red", size = 4)
